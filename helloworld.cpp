dd4b18541f848f250e7a1192d716086297284382
Kevin Alderman <too.many.namespaces@gmail.com>
2019-07-04 13:32:37 -0400

    works on my machine

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <string>
 
+#include <glbinding/gl/gl.h>
+#include <glbinding/Binding.h>
+#include <GLFW/glfw3.h>
+
 using namespace std;
 
 int main()
 {
-    cout << "hello world";
+    cout << "hello world" << endl;
     return 0;
 }
