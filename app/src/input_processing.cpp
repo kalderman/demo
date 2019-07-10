9e105c44fc1bbc4be26876effdbf2ac7cf3800ef
Kevin Alderman <too.many.namespaces@gmail.com>
2019-07-10 13:45:47 -0400

    refactored without unit test coverage tsk tsk

--- app/src/input_processing.cpp
+++ app/src/input_processing.cpp
@@ -2,12 +2,13 @@
 #include "advance_turtle.h"
 #include "turtle_events.hpp"
 #include <GLFW/glfw3.h>
+#include <optional>
 
 void update_turtle_log(vector<Turtle>& turtle_log, const variant<Move, Rotate, TogglePen>& event){
     turtle_log.push_back(advance_turtle(turtle_log.back(), event));
 }
 
-variant<Move, Rotate, TogglePen> convert_input_to_turtle_event(int key, int action){
+optional<variant<Move, Rotate, TogglePen>> convert_input_to_turtle_event(int key, int action){
     auto press_or_repeat = (action == GLFW_PRESS || action == GLFW_REPEAT);
     if (key == GLFW_KEY_SPACE && press_or_repeat)
         return TogglePen();
@@ -17,10 +18,10 @@ variant<Move, Rotate, TogglePen> convert_input_to_turtle_event(int key, int acti
         return Rotate(Rotate::Counterclockwise());
     if (key == GLFW_KEY_RIGHT && press_or_repeat)
         return Rotate(Rotate::Clockwise());
+    return {};
 }
 
 void update_turtle_log(int key, int action, vector<Turtle>& turtle_log){
-    update_turtle_log(
-        turtle_log,
-        convert_input_to_turtle_event(key, action));
+    auto maybe_event = convert_input_to_turtle_event(key, action);
+    if(maybe_event.has_value()) update_turtle_log(turtle_log, maybe_event.value());
 }
